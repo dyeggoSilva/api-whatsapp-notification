3e445dd9b997020e73ebf78292469306caae44db
dyeggoSilva <dyeggo0102@live.com>
2025-05-17 14:57:57 -0300

    :sparkles:	feat criação do service

--- src/main/java/com/dy/api_whatsapp_notification/controllers/NotificationControllerController.java
+++ src/main/java/com/dy/api_whatsapp_notification/controllers/NotificationControllerController.java
@@ -1,10 +1,22 @@
 package com.dy.api_whatsapp_notification.controllers;
 
+import com.dy.api_whatsapp_notification.services.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/notifications")
 
 public class NotificationControllerController {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @PostMapping("/send")
+    public String sendNotification(@RequestParam String phoneNumber, @RequestParam String message) {
+        return notificationService.sendWhatsAppMessage(phoneNumber, message);
+    }
 }
