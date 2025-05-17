71263aa9e0566f074b794af4b9adcc4e35594c54
dyeggoSilva <dyeggo0102@live.com>
2025-05-17 13:41:28 -0300

    :tada: init: criação do Projeto Spring

---
+++ src/main/java/com/dy/api_whatsapp_notification/ApiWhatsappNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.dy.api_whatsapp_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWhatsappNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWhatsappNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dy/api_whatsapp_notification/ApiWhatsappNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dy.api_whatsapp_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWhatsappNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
